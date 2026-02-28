d2e990d76b41e855ae4709e1119d8b590ea68bfb
Larissa Oliveira <lrssplx@gmail.com>
2026-02-27 23:26:04 -0300

    chore: auto commit via QA Panel - 2026-02-27 23:26:04

--- cypress/e2e/google.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Busca UOL', () => {
-  it('Deve pesquisar algo', () => {
-    cy.visit('/'); 
-    cy.get('.blackBar__listProducts > :nth-child(2) > .hyperlink').contains("Bate-Papo");
-         
-  });
-});
